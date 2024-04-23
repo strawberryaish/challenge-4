bbecec1f320269a03b022d0f339167a5640f47cf
strawberryaish <trismorgenstern@gmail.com>
2024-04-24 03:53:28 +0800

    add eventlistener and hidden class

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const popUpMessage = document.querySelector('.success-container');
+const popupContainer = document.querySelector('.success-container');
+const signupContainer = document.querySelector('.container')
+const subscribeBtn = document.querySelector('.subscribe-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+document.addEventListener('DOMContentLoaded', () => popupContainer.classList.add('hidden'));
+
+subscribeBtn.addEventListener('click', e => {
+    e.preventDefault();
+    
+    popupContainer.classList.remove('hidden');
+    signupContainer.classList.add('hidden');
+    
+
+    console.log('d');
+  
+});
+
+
+closeBtn.addEventListener('click', e => {
+    e.preventDefault();
+    
+    popupContainer.classList.add('hidden');
+    signupContainer.classList.remove('hidden');
+
+});
+
